c6f01a0f374f10e20eb0751f928819b584d412be
Subham <subham@reckonsys.com>
2020-05-28 19:26:51 +0530

    testing email

--- static/server.js
+++ static/server.js
@@ -50,10 +50,10 @@ app.post("/send", (req, res) => {
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "/")));
+app.use(express.static(path.join(__dirname, "../")));
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "../index.html"));
 });
 
 const port = process.env.PORT || 5000;
