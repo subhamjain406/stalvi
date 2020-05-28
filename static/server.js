f7a4a59f81844b01ac591f8798387880aebf0af7
Subham <subham@reckonsys.com>
2020-05-28 23:13:27 +0530

    reback

--- static/server.js
+++ static/server.js
@@ -38,7 +38,7 @@ apiKey += "ITRNQ0nampZZCIEMz-";
 apiKey += "jdzI9S6CpCHb";
 apiKey += "bOZo6XFJnZ57o";
 
-app.post("*", (req, res) => {
+app.post("/send", (req, res) => {
   const output = `
   <ul>
   <li> name : ${req.body.first_name} ${req.body.last_name}</li>
